777bf11c44e7e8b61511ea52c07a9a6b53584c82
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2025-06-01 18:52:23 -0700

    revert to CrdTransf

--- SRC/opensees/openseespy.pyi
+++ SRC/opensees/openseespy.pyi
@@ -1,26 +1,46 @@
 from typing import overload, Literal
 
 class _Elements:
+
+    @overload
+    def element(self, type: str, tag: int, nodes: tuple, **kwargs) -> int:
+        """
+        Create an element of the specified type.
+        """
+
     @overload
     def element(self,
                 __elmType: Literal["PrismFrame"],
                 tag: int,
                 nodes: tuple,
-                secTag: int,
+                section: int,
+                transform: int,
                 *args
-               ) -> int: ...
+            ) -> int:
+        """
+        Create a linear elastic prismatic frame element.        
+        
+        :param tag: unique :ref:`element` tag
+        :type tag: |integer|
+        :param nodes: tuple of *two* integer :ref:`node` tags
+        :type nodes: tuple
+        :param section: tag of a previously-defined :ref:`section`
+        :type section: |integer|
+        :param transform: identifier for previously-defined coordinate-transformation
+        :type transform: |integer|
+        """
+
     @overload
     def element(self,
                 __elmType: Literal["forceBeamColumn"],
                 tag: int,
                 nodes: tuple,
+                nip: int,
                 transform: int,
-                nInts: int,
-                pts: list,
                 *args
                ) -> int: ...
 
-    def element(self, *args, **kwargs) -> int: ...
+
 
 class Model(_Elements):
     pass
